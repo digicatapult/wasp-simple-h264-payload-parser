08fb528a4c17b4c3ebb6cd044ef5da9e8870a7c9
Daniel Cooper <danielcooperxyz@gmail.com>
2022-03-16 16:28:39 +0000

    Update tests to be representative

--- wasp/video-message-transform_test.go
+++ wasp/video-message-transform_test.go
@@ -5,22 +5,19 @@ import (
 	"testing"
 
 	"github.com/digicatapult/wasp-simple-h264-payload-parser/wasp"
-	uuid "github.com/satori/go.uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
 func TestTransformVideoMessage(t *testing.T) {
 	ingest := &wasp.IngestMessage{
-		Ingest:    uuid.NewV4().String(),
-		IngestID:  uuid.NewV4().String(),
-		Timestamp: uuid.NewV4().String(),
-		Payload:   uuid.NewV4().String(),
-		ThingID:   uuid.NewV4().String(),
-		Type:      uuid.NewV4().String(),
-		Metadata: map[string]interface{}{
-			uuid.NewV4().String(): uuid.NewV4().String(),
-		},
+		ThingID:   "883d818a-e115-4b99-b264-bb2033de21f1",
+		Type:      "h264",
+		Ingest:    "ingest-rtmp",
+		IngestID:  "APP/STREAM",
+		Payload:   "bytes",
+		Timestamp: "2021-03-23 00:00:00", // iso date example
+		Metadata:  map[string]interface{}{},
 	}
 
 	msgBytes, err := json.Marshal(ingest)
@@ -38,6 +35,5 @@ func TestTransformVideoMessage(t *testing.T) {
 	assert.Equal(t, ingest.Type, output.Type)
 	assert.Equal(t, ingest.Payload, output.Value)
 	assert.Equal(t, ingest.Timestamp, output.Timestamp)
-	assert.Equal(t, ingest.ThingID, output.ThingID)
 	assert.Equal(t, ingest.Metadata, output.Metadata)
 }
